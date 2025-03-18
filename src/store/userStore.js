10dd781aa1866895879ab034cbef89fa979c6a7a
Jose Manuel Oberreuter <jose.manuel.oberreuter@gmail.com>
2025-03-18 17:18:22 -0300

    feat: pinia store

---
+++ src/store/userStore.js
@@ -0,0 +1,39 @@
+import { defineStore } from 'pinia';
+import axios from 'axios';
+
+export const useUserStore = defineStore('user', {
+  state: () => ({
+    token: localStorage.getItem("token") || null, // Cargar el token desde localStorage
+    userData: null,
+  }),
+  actions: {
+    setToken(token) {
+      this.token = token;
+      localStorage.setItem("token", token); // Guardar el token en localStorage
+    },
+    setUserData(userData) {
+      this.userData = userData;
+    },
+    clearUserData() {
+      this.token = null;
+      this.userData = null;
+      localStorage.removeItem("token"); // Eliminar el token de localStorage
+    },
+    async fetchUserData() {
+      if (this.token) {
+        try {
+          const response = await axios.get("http://localhost:4005/users/user-data", {
+            headers: {
+              Authorization: `Bearer ${this.token}`,
+            },
+          });
+          this.setUserData(response.data); // Guardar los datos del usuario
+          console.log("Datos del usuario cargados:", response.data);
+        } catch (error) {
+          console.error("Error al cargar los datos del usuario:", error);
+          this.clearUserData(); // Limpiar los datos si hay un error
+        }
+      }
+    },
+  },
+});
